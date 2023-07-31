60c038c95b77e7e811579f1e7f334cd6e2cf4ddd
Nick de Wijer <nick.dewijer@softwareone.com>
2023-07-31 10:33:23 +0200

    first script

---
+++ README.md
@@ -0,0 +1,17 @@
+# python-ha-metrics-to-dynamodb
+
+This python script will run through an home-assistant database and grabs the defined metrics, then puts them in a AWS DynamoDB database.
+
+## Functionality
+1. Connects to AWS and SQLite database to also test functionality
+2. Gets metadata_id based on defined sensors from the sqlite database
+3. Gets timestamp from SSM Parameter store.
+4. Polls all metrics past the defined timestamp from the sqlite database
+5. Puts the records into DynamoDB and reports its progress.
+6. Saves final timestamp in SSM Parameter store. 
+
+## Requirements
+   * SSM Parameter to read from and store the timestamp of where to pick-up from
+   * DynamoDB to write records too with metadata_id as the hash and timestamp as the range key.
+   * IAM credentials to do the above.
+
---
+++ main.py
@@ -0,0 +1,138 @@
+from decimal import Decimal, DecimalException
+import sqlite3
+import boto3
+from botocore.exceptions import ClientError
+import logging
+import sys
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+metadata_ids = []
+
+if __name__ == '__main__':
+
+    # DEFINE Variables
+    awsregion = "eu-west-1"
+    ssm_parameter_timestamp = "electricity-usage-timestamp"
+    sqlite_database = "home-assistant_v2.db"
+    dynamodb_database = "ddbtable-electricity"
+
+    # DEFINE Sensors to poll
+    sensors = [
+        "sensor.electricity_meter_energy_consumption_tarif_1",
+        "sensor.electricity_meter_energy_consumption_tarif_2",
+        "sensor.electricity_meter_energy_production_tarif_1",
+        "sensor.electricity_meter_energy_production_tarif_2",
+        "sensor.solaredge_ac_power",
+        "sensor.solaredge_ac_energy_kwh"
+    ]
+
+    try:
+        # Connect AWS DynamoDB and SSM
+        session = boto3.Session(profile_name='default')
+        ddb = session.resource('dynamodb', region_name=awsregion)
+        table = ddb.Table(dynamodb_database)
+        ssm = session.client('ssm', region_name=awsregion)
+    except ClientError as err:
+        logger.error(
+            "Couldn't connect to DynamoDB or SSM. Here's why: %s: %s",
+            err.response['Error']['Code'], err.response['Error']['Message'])
+        raise
+
+    # get ssm parameter for dynamodb timestamp
+    try:
+        res = ssm.get_parameter(
+            Name=ssm_parameter_timestamp, WithDecryption=False)
+        timestamp = res['Parameter']['Value']
+    except ClientError as err:
+        logger.error(
+            "Couldn't get timestamp from SSM. Here's why: %s: %s",
+            err.response['Error']['Code'], err.response['Error']['Message'])
+        raise
+
+    # Connect to sqllite3
+    try:
+        con = sqlite3.connect(sqlite_database)
+        cur = con.cursor()
+    except sqlite3.Error as err:
+        logger.error(
+            "Couldn't connect to sqlite3. Here's why: %s: %s",
+            err.response['Error']['Code'], err.response['Error']['Message'])
+        raise
+
+    # Get metadata_ids
+    for sensor in sensors:
+        try:
+            res = cur.execute(
+                "SELECT metadata_id FROM states_meta WHERE entity_id = ?", (sensor,))
+            metadata_id = res.fetchone()[0]
+            metadata_ids.append(metadata_id)
+        except sqlite3.Error as err:
+            logger.error(
+                "Couldn't get metadata_id from sqlite3. Here's why: %s: %s",
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+
+    # Using metadata ids, get all the sensor data.
+    try:
+        cur.execute("SELECT state, last_updated_ts, metadata_id FROM states where metadata_id in {} and "
+                    "last_updated_ts > {}".format(str(tuple(metadata_ids)), timestamp))
+        rows = cur.fetchall()
+        logger.info("total records found: %s", len(rows))
+    except sqlite3.Error as err:
+        logger.error(
+            "Couldn't get data from sqlite3. Here's why: %s: %s",
+            err.response['Error']['Code'], err.response['Error']['Message'])
+        raise
+
+    # Batch writer allows for fast entries into DynamoDB
+    with table.batch_writer() as batch:
+        for idx, row in enumerate(rows):
+            try:
+                # In certain situations, the value of state will be unknown which decimal cannot handle.
+                try:
+                    state = Decimal(row[0])
+                except (ValueError, DecimalException):
+                    state = Decimal(0)
+                    pass
+
+                content = {
+                    'metadata_id': row[2],
+                    'state': state,
+                    'last_updated_ts': str(row[1])
+                }
+                batch.put_item(Item=content)
+            except ClientError as err:
+                logger.error(
+                    "Couldn't add entry with timestamp %s to table %s. Here's why: %s: %s",
+                    str(row[1]), table.name,
+                    err.response['Error']['Code'], err.response['Error']['Message'])
+                try:
+                    # write timestamp to ssm parameter to try again next time
+                    ssm.put_parameter(Name=ssm_parameter_timestamp,
+                                      Value=timestamp, Overwrite=True)
+                except ClientError as err:
+                    logger.error(
+                        "Double error. Could not save up to date timestamp to SSM Parameter. Please be wary of "
+                        "duplicates. Timestamp: %s. Error: %s: %s",
+                        str(row[1]), err.response['Error']['Code'], err.response['Error']['Message'])
+                raise
+
+            # Each 25 records, write status to logger.
+            if ((idx + 1) % 25 == 0) or ((idx + 1) == len(rows)):
+                logger.info("Added %s entries to DynamoDB table. Latest timestamp: %s", str(
+                    idx + 1), str(row[1]))
+            timestamp = str(row[1])
+    try:
+        ssm.put_parameter(Name=ssm_parameter_timestamp,
+                          Value=timestamp, Overwrite=True)
+        logger.info(
+            "Finished. Updated SSM Parameter with latest timestamp: %s", timestamp)
+    except ClientError as err:
+        logger.error(
+            "Could not save up to date timestamp to SSM Parameter. Please be wary of "
+            "duplicates. Timestamp: %s. Error: %s: %s",
+            str(row[1]), err.response['Error']['Code'], err.response['Error']['Message'])
+        raise
